5beab07a60aee99fa35f88a849101d0014d414e1
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-07-31 22:34:32 +0530

    Create countplot.py

---
+++ seaborn/countplot.py
@@ -0,0 +1,10 @@
+code:
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+sns.set_theme(style="dark",palette="colorblind")
+df = pd.DataFrame({
+    "Age": [25, 30, 35, 40],
+    "c": [180, 190, 210, 220],
+    "hd": ["No", "No", "No", "Yes"]})
+sns.countplot(data=df,x="hd")
