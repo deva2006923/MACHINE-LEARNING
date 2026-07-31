fe0ca69b30a0d340b2ec19d0d1a7f78ed8f6886f
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-07-31 21:03:06 +0530

    Create head.py

---
+++ pandas/head.py
@@ -0,0 +1,22 @@
+code:
+     c = pd.read_csv("/content/sample_data/california_housing_train.csv")
+     c.head()   # RETURNS THE FIRST 5 ROWS
+
+
+
+output:
+       longitude  latitude  housing_median_age  total_rooms  total_bedrooms  \
+0    -114.31     34.19                15.0       5612.0          1283.0   
+1    -114.47     34.40                19.0       7650.0          1901.0   
+2    -114.56     33.69                17.0        720.0           174.0   
+3    -114.57     33.64                14.0       1501.0           337.0   
+4    -114.57     33.57                20.0       1454.0           326.0   
+
+   population  households  median_income  median_house_value  
+0      1015.0       472.0         1.4936             66900.0  
+1      1129.0       463.0         1.8200             80100.0  
+2       333.0       117.0         1.6509             85700.0  
+3       515.0       226.0         3.1917             73400.0  
+4       624.0       262.0         1.9250             65500.0 
+
+   
