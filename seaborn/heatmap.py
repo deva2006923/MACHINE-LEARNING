675a0637dfe29f82f42c1a802bdc7168f716496f
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-07-31 22:44:09 +0530

    Create heatmap.py

---
+++ seaborn/heatmap.py
@@ -0,0 +1,14 @@
+code:
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+sns.set_theme(style="dark",palette="colorblind")
+df = pd.DataFrame({
+    "Age":[20,25,30,35],
+    "BP":[120,130,140,150],
+    "Cholesterol":[180,190,200,220],
+    "Gender":["M","F","M","F"]
+})
+
+sns.heatmap(df.corr(numeric_only=True),annot=True,cmap="Blues",linewidths=0.2,linecolor="green",square=True);
+plt.show()
